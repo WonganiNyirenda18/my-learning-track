4d785aeaead2dd236c1b33e7482bb680f859b9bc
wongani18 <wonganinyirenda18@gmail.com>
2026-01-08 12:59:56 +0200

    feat: added basic logic of count

--- src/Features/UseState.jsx
+++ src/Features/UseState.jsx
@@ -1,7 +1,21 @@
 export default function UseState() {
+
+    let count = 0;
+
+    function increaseCount() {
+        count += 1;
+
+        console.log(count);
+    }
+
     return (
         <>
             <h1>UseState</h1>
+
+            <div>
+                <p>Count: {count}</p>
+                <button onClick={increaseCount}>Increase Count</button>
+            </div>
         </>
     );
 }
