bd2fd28fe5ff2bf9e4dfdf79a5de2b1dd818ea52
wongani18 <wonganinyirenda18@gmail.com>
2026-01-08 13:10:49 +0200

    feat: implemented the useState hook

--- src/Features/UseState.jsx
+++ src/Features/UseState.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 export default function UseState() {
 
-    let count = 0;
+    // let count = 0;
 
-    function increaseCount() {
-        count += 1;
+    /* UseState
+    place an initial values either a number or a string or an object or an array.
+    useStae() was built to return an array of two elements.
+    first element is the current state value.
+    second element is the function that updates the state value.*/
+
+    const [count, setCount] = useState(0);
+
+    const increaseCount = () => {
+        setCount((prev) => prev + 1);
 
         console.log(count);
     }
