cd9df129bb011366a786e965dc8c0f5c8348b681
wongani18 <wonganinyirenda18@gmail.com>
2026-01-19 12:33:41 +0200

    feat: fixed react logic

--- src/Features/Callback.jsx
+++ src/Features/Callback.jsx
@@ -1,19 +1,22 @@
 export default function Callback() {
+
+
+    {/*basic understanding of callbacks*/ }
+    function greet(name, callback) {
+        console.log("Hello, " + name);
+        callback();
+    }
+
+    function sayHie() {
+        console.log("Goodbye");
+    }
+
+    greet("John", sayHie);
+
     return (
         <div>
             <h1>Callback</h1>
 
-            {/*basic understanding of callbacks*/}
-            function greet(name, callback) {
-                console.log("Hello, " + name);
-            callback();
-            }
-
-            function sayHie() {
-                console.log("Goodbye");
-            }
-
-            greet("John", sayHie);
         </div>
     );
 }
