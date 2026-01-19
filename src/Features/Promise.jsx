95a6d44ec018831139cad2a1a07a351a8aa3330c
wongani18 <wonganinyirenda18@gmail.com>
2026-01-19 14:35:05 +0200

    feat: fixed the promise syntax

--- src/Features/Promise.jsx
+++ src/Features/Promise.jsx
@@ -1,26 +1,32 @@
-export default function Promise() {
-    {/*basic understanding of promises*/ }
+import { useEffect } from "react";
 
-    let promise = new Promise((resolve, reject) => {
-        let number = 4;
-        if (number % 2 == 0) {
-            resolve("Number is even")
-        } else {
-            reject("Number is odd")
-        }
-    });
+export default function PromiseExample() {
 
-    checkEven
-        .then((message) => {
-            console.log(message)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+    useEffect(() => {
+        let promise = new Promise((resolve, reject) => {
+            let number = 4;
+
+            if (number % 2 === 0) {
+                resolve("Number is even");
+            } else {
+                reject("Number is odd");
+            }
+        });
+
+        promise
+            .then((message) => {
+                console.log(message);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+    }, []); // runs once when component loads
 
     return (
-        <>
+        <div>
             <h1>Promise</h1>
-        </>
+            <p>Check the console for output</p>
+        </div>
     );
 }
