9d8dcf492d44659bbabee882d1c7d8dbb79b9734
wongani18 <wonganinyirenda18@gmail.com>
2026-01-19 14:23:08 +0200

    feat: added basic promise syntax

--- src/Features/Promise.jsx
+++ src/Features/Promise.jsx
@@ -1,4 +1,23 @@
 export default function Promise() {
+    {/*basic understanding of promises*/ }
+
+    let promise = new Promise((resolve, reject) => {
+        let number = 4;
+        if (number % 2 == 0) {
+            resolve("Number is even")
+        } else {
+            reject("Number is odd")
+        }
+    });
+
+    checkEven
+        .then((message) => {
+            console.log(message)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+
     return (
         <>
             <h1>Promise</h1>
