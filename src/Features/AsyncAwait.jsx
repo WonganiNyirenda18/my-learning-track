337da48766d1414c1428e2b4a2504dd025bebdda
wongani18 <wonganinyirenda18@gmail.com>
2026-01-21 14:34:37 +0200

    feat: add AsyncAwait component demonstrating async/await functionality with error handling.

--- src/Features/AsyncAwait.jsx
+++ src/Features/AsyncAwait.jsx
@@ -1,4 +1,28 @@
 export default function AsyncAwait() {
+
+    function alarm(name, delay) {
+        return new Promise((resolve, reject) => {
+            if (delay < 0) {
+                reject("Invalid delay")
+            }
+
+            setTimeout(() => {
+                resolve(`Wake up ${name}`)
+            }, delay);
+        });
+    }
+
+    async function setAlarm(name, delay) {
+        try {
+            const message = await alarm(name, delay);
+            console.log(message);
+        } catch (error) {
+            console.log("could not set alarm", error);
+        }
+    }
+
+    setAlarm("John", -2000);
+
     return (
         <>
             <h1>Async/Await</h1>
