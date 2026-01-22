48a29cf439514900d184bbf941319c35ed89db8e
wongani18 <wonganinyirenda18@gmail.com>
2026-01-22 14:49:20 +0200

    added the useEffect basic structure

--- src/Features/UseEffect.jsx
+++ src/Features/UseEffect.jsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from "react";
+
 export default function UseEffect() {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log("count is now", count)
+    }, [count]);
+
     return (
         <>
             <h1>UseEffect</h1>
+            <button onClick={() => setCount(count + 1)}>Count: {count}</button>
         </>
     );
 }
