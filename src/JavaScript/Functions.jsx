62c4bd269a9a66dc181467f47550dd3684f3abf2
wongani18 <wonganinyirenda18@gmail.com>
2026-02-16 12:01:42 +0200

    feat: implemented a function with parameters

--- src/JavaScript/Functions.jsx
+++ src/JavaScript/Functions.jsx
@@ -1,10 +1,21 @@
 export default function Functions() {
 
-    const text = function () {
+    /*const text = function () {
         console.log("My name is Wongani Nyirenda");
     }
 
-    text();
+    text();*/
+
+    const power = function (base, exponent) {
+        let result = 1;
+        for (let count = 0; count < exponent; count++) {
+            result *= base;
+        }
+
+        return result;
+    };
+
+    console.log(power(2, 10));
 
     return (
         <>
