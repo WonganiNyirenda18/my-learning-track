ee681fe86f172c722a9218f87c4fe21e2d079137
wongani18 <wonganinyirenda18@gmail.com>
2026-02-16 11:52:46 +0200

    feeat: implemented the first function without parameters

--- src/JavaScript/Functions.jsx
+++ src/JavaScript/Functions.jsx
@@ -1,7 +1,13 @@
-export function Functions() {
+export default function Functions() {
+
+    const text = function () {
+        console.log("My name is Wongani Nyirenda");
+    }
+
     return (
         <>
             <h1>Learning functions</h1>
+            console.log(text);
         </>
     )
 }
