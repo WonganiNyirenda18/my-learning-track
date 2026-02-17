221ef1f183e88f538d24aca3c4b9f3fc153859c2
wongani18 <wonganinyirenda18@gmail.com>
2026-02-17 10:17:35 +0200

    learning binding and scopes

--- src/JavaScript/Functions.jsx
+++ src/JavaScript/Functions.jsx
@@ -6,7 +6,7 @@ export default function Functions() {
 
     text();*/
 
-    const power = function (base, exponent) {
+    /* const power = function (base, exponent) {
         let result = 1;
         for (let count = 0; count < exponent; count++) {
             result *= base;
@@ -15,7 +15,19 @@ export default function Functions() {
         return result;
     };
 
-    console.log(power(2, 10));
+    console.log(power(2, 10)); */
+
+    //Binding and Scopes
+
+    let x = 10;
+
+    if (true) {
+        let y = 20;
+        var z = 30;
+        console.log(x + y + z);
+    }
+
+
 
     return (
         <>
